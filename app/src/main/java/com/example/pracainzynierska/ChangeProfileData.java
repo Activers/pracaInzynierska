9814f6cba7851c92c49568f4b5d7f695d0f3f761
Mateusz Sz <matix2011@gmail.com>
2021-04-24 19:54:44 +0200

    Wprowadzanie danych gier użytkownika

---
+++ app/src/main/java/com/example/pracainzynierska/ChangeProfileData.java
@@ -0,0 +1,14 @@
+package com.example.pracainzynierska;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangeProfileData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_profile_data);
+    }
+}
