0fdda4078842536acd321a935d599feabae4b929
Activers <gruzewski.tom@gmail.com>
2021-04-24 21:03:17 +0200

    Zrobione: Layout Ustawienia, zmiana hasła(wraz ze zmianą danych autologowania)

--- app/src/main/java/com/example/pracainzynierska/ResetPassword.java
+++ app/src/main/java/com/example/pracainzynierska/ResetPassword.java
@@ -3,7 +3,7 @@ package com.example.pracainzynierska;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -14,13 +14,14 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class ResetPassword extends AppCompatActivity {
 
-    EditText Password,RepeatPass;
-    Button NewPassword;
+    EditText NewPassword, RepeatNewPass, OldPassword;
+    Button ChangePassword;
     FirebaseAuth fAuth;
 
     @Override
@@ -28,47 +29,68 @@ public class ResetPassword extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
 
-        Password = findViewById(R.id.editTextNewPassword);
-        RepeatPass = findViewById(R.id.editTextRepeatNewPass);
-        NewPassword = findViewById(R.id.buttonNewPassword);
+        OldPassword = findViewById(R.id.editTextOldPassword);
+        NewPassword = findViewById(R.id.editTextNewPassword);
+        RepeatNewPass = findViewById(R.id.editTextRepeatNewPass);
+        ChangePassword = findViewById(R.id.buttonChangeOldPassword);
         fAuth = FirebaseAuth.getInstance();
 
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        NewPassword.setOnClickListener(new View.OnClickListener() {
+        ChangePassword.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
 
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                final String TAG = "Password";
-
-                String password = Password.getText().toString();
-                String repeatPass = RepeatPass.getText().toString();
-                if (TextUtils.isEmpty(password)) {
-                    Password.setError("To pole jest wymagane!");
-                    return;
-                }
-                if (!password.equals(repeatPass)){
-                    RepeatPass.setError("Hasła nie są takie same!!");
-                    return;
-                }
-
-                user.updatePassword(password).addOnCompleteListener(new OnCompleteListener<Void>() {
+                fAuth.signInWithEmailAndPassword(user.getEmail(), OldPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onComplete(@NonNull Task<Void> task) {
+                    public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Log.d(TAG, "Haslo zaktualizowanie");
-                            Toast.makeText(ResetPassword.this, "Hasło zostało zmienione!", Toast.LENGTH_SHORT).show();
-                            onBackPressed();
-                        } else {
-                            Toast.makeText(ResetPassword.this, "Błąd zmiany hasła!", Toast.LENGTH_SHORT).show();
-                            Log.d(TAG, "Error! Haslo nie zaktualizowane");
+
+                            final String TAG = "Password";
+
+
+                            final String newPassword = NewPassword.getText().toString().trim();
+                            String repeatNewPass = RepeatNewPass.getText().toString().trim();
+                            if (TextUtils.isEmpty(newPassword)) {
+                                NewPassword.setError("To pole jest wymagane!");
+                                return;
+                            }
+                            if (!newPassword.equals(repeatNewPass)) {
+                                RepeatNewPass.setError("Hasła nie są takie same!!");
+                                return;
+                            }
+
+                            user.updatePassword(newPassword).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+
+                                        // podmiana SharedPreferences
+                                        String AUTO_LOGIN_PREF_NAME = getString(R.string.autoLoginPreferenceName); // nazwa preferencji / pliku gdzie skladowane beda klucz-wartosc
+                                        SharedPreferences preferences = getSharedPreferences(AUTO_LOGIN_PREF_NAME, MODE_PRIVATE);
+                                        if (preferences.contains("pref_automaticLogin")) {
+                                            SharedPreferences.Editor editor = preferences.edit();
+                                            editor.putString("pref_password", newPassword);
+                                            editor.apply();
+                                            Log.i(TAG, "Zmieniono hasło autologowania - zapisano password");
+                                        }
+                                        // koniec podmianki
+
+                                        Log.d(TAG, "Hasło zaktualizowanie");
+                                        Toast.makeText(ResetPassword.this, "Hasło zostało zmienione!", Toast.LENGTH_SHORT).show();
+                                        onBackPressed();
+                                    } else {
+                                        Toast.makeText(ResetPassword.this, "Błąd zmiany hasła!", Toast.LENGTH_SHORT).show();
+                                        Log.d(TAG, "Error! Haslo nie zaktualizowane");
+                                    }
+                                }
+                            });
                         }
+                        else { OldPassword.setError("Podano nieprawidłowe stare hasło!"); }
                     }
                 });
-
             }
         });
-
 
     }
 
