69b435baa2f9eae24c6708c44ae1a052bfb466d9
Mateusz Sz <matix2011@gmail.com>
2021-04-25 20:43:43 +0200

    Poprawiony layout ustawien i zmian danych uzytkownika

--- app/src/main/java/com/example/pracainzynierska/Settings.java
+++ app/src/main/java/com/example/pracainzynierska/Settings.java
@@ -6,18 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class Settings extends AppCompatActivity {
 
-    Button Profile,Password;
+    LinearLayout Profile,Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        Profile =findViewById(R.id.buttonChangeProfile);
-        Password = findViewById(R.id.buttonChangePassword);
+        Profile =findViewById(R.id.ChangeProfile);
+        Password = findViewById(R.id.ChangePassword);
+
+        Profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),ChangeProfileData.class));
+            }
+        });
 
         Password.setOnClickListener(new View.OnClickListener() {
             @Override
